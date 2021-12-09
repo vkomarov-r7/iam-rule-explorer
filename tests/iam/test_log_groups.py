7779e67c935769e3a9151d975eeb18bd5a36f1b9
Nadia Mounzih <nadia_mounzih@rapid7.com>
2021-12-09 16:51:47 -0500

    Updated tests in test_log_groups.py

--- tests/iam/test_log_groups.py
+++ tests/iam/test_log_groups.py
@@ -23,7 +23,29 @@ def log_group(policy_executor):
 
 
 def test_whole_arn(policy_executor, log_group):
-    """Test arn matching with log groups. This test is an exact ARN match"""
+    """Test arn matching with log groups. This is a full match to the log group ARN"""
+    policy_executor.set_role_policy({
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "statement1",
+                "Effect": "Allow",
+                "Action": [
+                    "logs:List*",
+                ],
+                "Resource": f"arn:aws:logs:us-east-1:050283019178:log-group:{log_group}:*",
+            }
+        ]
+    })
+
+    client = policy_executor.roleclient('logs')
+    client.list_tags_log_group(
+        logGroupName=log_group
+    )
+
+
+def test_arn_with_stream(policy_executor, log_group):
+    """Test arn matching with log groups. This test has a stream"""
     policy_executor.set_role_policy({
         "Version": "2012-10-17",
         "Statement": [
@@ -56,28 +78,6 @@ def test_arn_star_end(policy_executor, log_group):
                     "logs:List*",
                 ],
                 "Resource": f"arn:aws:logs:us-east-1:050283019178:log-group:{log_group}:*:",
-            }
-        ]
-    })
-
-    client = policy_executor.roleclient('logs')
-    client.list_tags_log_group(
-        logGroupName=log_group
-    )
-
-
-def test_arn_star_end_no_colon(policy_executor, log_group):
-    """Test arn matching with log groups. The log group arn in question ends in log-stream (no star, see first test)"""
-    policy_executor.set_role_policy({
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Sid": "statement1",
-                "Effect": "Allow",
-                "Action": [
-                    "logs:List*",
-                ],
-                "Resource": f"arn:aws:logs:us-east-1:050283019178:log-group:{log_group}:*",
             }
         ]
     })
