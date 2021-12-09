115b24da82d8eaa3a29a00170a9ca27f9d6fe193
Nadia Mounzih <nadia_mounzih@rapid7.com>
2021-12-09 09:23:29 -0500

    Added new test file for log groups

---
+++ tests/iam/test_log_groups.py
@@ -0,0 +1,133 @@
+import pytest
+from uuid import uuid4
+import logging
+from util import raises_boto_code
+
+
+LOG = logging.getLogger(__name__)
+
+
+@pytest.fixture
+def log_group(policy_executor):
+    client = policy_executor.superclient('logs')
+    log_group_name = f'iam-arn-test-{uuid4()}'
+
+    client.create_log_group(logGroupName=log_group_name,
+                            tags={'testing': 'tags'})
+    LOG.info("Created log group: %s", log_group_name)
+
+    yield log_group_name
+
+    LOG.info("Deleting log group: %s", log_group_name)
+    client.delete_log_group(logGroupName=log_group_name)
+
+
+def test_whole_arn(policy_executor, log_group):
+    """Test arn matching with log groups. This test is an exact ARN match"""
+    policy_executor.set_role_policy({
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "statement1",
+                "Effect": "Allow",
+                "Action": [
+                    "logs:List*",
+                ],
+                "Resource": f"arn:aws:logs:us-east-1:050283019178:log-group:{log_group}:log-stream:",
+            }
+        ]
+    })
+
+    client = policy_executor.roleclient('logs')
+    client.list_tags_log_group(
+        logGroupName=log_group
+    )
+
+
+def test_arn_star_end(policy_executor, log_group):
+    """Test arn matching with log groups. The log group arn in question ends in log-stream (no star, see test above)"""
+    policy_executor.set_role_policy({
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "statement1",
+                "Effect": "Allow",
+                "Action": [
+                    "logs:List*",
+                ],
+                "Resource": f"arn:aws:logs:us-east-1:050283019178:log-group:{log_group}:*:",
+            }
+        ]
+    })
+
+    client = policy_executor.roleclient('logs')
+    client.list_tags_log_group(
+        logGroupName=log_group
+    )
+
+
+def test_arn_star_end_no_colon(policy_executor, log_group):
+    """Test arn matching with log groups. The log group arn in question ends in log-stream (no star, see first test)"""
+    policy_executor.set_role_policy({
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "statement1",
+                "Effect": "Allow",
+                "Action": [
+                    "logs:List*",
+                ],
+                "Resource": f"arn:aws:logs:us-east-1:050283019178:log-group:{log_group}:*",
+            }
+        ]
+    })
+
+    client = policy_executor.roleclient('logs')
+    client.list_tags_log_group(
+        logGroupName=log_group
+    )
+
+
+def test_arn_star_resource(policy_executor, log_group):
+    """Test arn matching with log groups. The log group arn in question ends in log-stream (no star, see first test)"""
+    policy_executor.set_role_policy({
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "statement1",
+                "Effect": "Allow",
+                "Action": [
+                    "logs:List*",
+                ],
+                "Resource": f"arn:aws:logs:us-east-1:050283019178:log_group:*",
+            }
+        ]
+    })
+
+    client = policy_executor.roleclient('logs')
+    with raises_boto_code('AccessDeniedException'):
+        client.list_tags_log_group(
+            logGroupName=log_group
+        )
+
+
+def test_arn_star_resource_type(policy_executor, log_group):
+    """Test arn matching with log groups. The log group arn in question ends in log-stream (no star, see first test)"""
+    policy_executor.set_role_policy({
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "statement1",
+                "Effect": "Allow",
+                "Action": [
+                    "logs:List*",
+                ],
+                "Resource": f"arn:aws:logs:us-east-1:050283019178:*",
+            }
+        ]
+    })
+
+    client = policy_executor.roleclient('logs')
+    client.list_tags_log_group(
+        logGroupName=log_group
+    )
