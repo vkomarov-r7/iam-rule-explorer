4988a7ea674e372edc78356f2e4ba5e0f64d2597
Nadia Mounzih <nadia_mounzih@rapid7.com>
2022-01-26 11:48:08 -0500

    Added new tests to log groups

--- tests/iam/test_log_groups.py
+++ tests/iam/test_log_groups.py
@@ -14,6 +14,7 @@ def log_group(policy_executor):
 
     client.create_log_group(logGroupName=log_group_name,
                             tags={'testing': 'tags'})
+
     LOG.info("Created log group: %s", log_group_name)
 
     yield log_group_name
@@ -61,9 +62,33 @@ def test_arn_with_stream(policy_executor, log_group):
     })
 
     client = policy_executor.roleclient('logs')
-    client.list_tags_log_group(
+    response = client.list_tags_log_group(
         logGroupName=log_group
     )
+    print(response)
+
+
+def test_arn_with_incorrect_stream(policy_executor, log_group):
+    """Test arn matching with log groups. This test has an incorrect word instead of log-stream"""
+    policy_executor.set_role_policy({
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "statement1",
+                "Effect": "Allow",
+                "Action": [
+                    "logs:List*",
+                ],
+                "Resource": f"arn:aws:logs:us-east-1:050283019178:log-group:{log_group}:burrito:",
+            }
+        ]
+    })
+
+    client = policy_executor.roleclient('logs')
+    with raises_boto_code('AccessDeniedException'):
+        client.list_tags_log_group(
+            logGroupName=log_group
+        )
 
 
 def test_arn_star_end(policy_executor, log_group):
@@ -88,7 +113,7 @@ def test_arn_star_end(policy_executor, log_group):
     )
 
 
-def test_arn_star_resource(policy_executor, log_group):
+def test_arn_star_resource_no_match(policy_executor, log_group):
     """Test arn matching with log groups. The log group arn in question ends in log-stream (no star, see first test)"""
     policy_executor.set_role_policy({
         "Version": "2012-10-17",
@@ -123,6 +148,50 @@ def test_arn_star_resource_type(policy_executor, log_group):
                     "logs:List*",
                 ],
                 "Resource": f"arn:aws:logs:us-east-1:050283019178:*",
+            }
+        ]
+    })
+
+    client = policy_executor.roleclient('logs')
+    client.list_tags_log_group(
+        logGroupName=log_group
+    )
+
+
+def test_arn_star_region(policy_executor, log_group):
+    """Test arn matching with log groups"""
+    policy_executor.set_role_policy({
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "statement1",
+                "Effect": "Allow",
+                "Action": [
+                    "logs:List*",
+                ],
+                "Resource": f"arn:aws:logs:*",
+            }
+        ]
+    })
+
+    client = policy_executor.roleclient('logs')
+    client.list_tags_log_group(
+        logGroupName=log_group
+    )
+
+
+def test_partial_group_name_arn(policy_executor, log_group):
+    """Test arn matching with log groups"""
+    policy_executor.set_role_policy({
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "statement1",
+                "Effect": "Allow",
+                "Action": [
+                    "logs:List*",
+                ],
+                "Resource": f"arn:aws:logs:us-east-1:050283019178:log-group:i*",
             }
         ]
     })
